79875441eda9a19b1433fa2fcf7aa73332e241c0
wangrongding <wangrongding@qq.com>
2021-10-12 01:32:40 +0800

    hold: 暂存

---
+++ scripts/clone-all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# 子仓库一
+
+echo "1234"
+# cd ../packages
+# git clone https://github.com/wangrongding/TF-boy.git
+
+# 子仓库二
+# cd packages && git https://github.com/wangrongding/image-processing.git
---
+++ scripts/pull.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+printf 'test'
+# 子仓库一
+# cd a && git pull
+
+# 子仓库二
+# cd b && git pull
